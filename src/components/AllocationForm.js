539fa520bc3d3c7c06b406892fb2b5f5b3bad65a
cloudbongee <jaimebeilism@outlook.com>
2023-08-12 11:28:49 -0700

    to end it i had to fix the alert problems

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -9,7 +9,7 @@ const AllocationForm = (props) => {
     const [action, setAction] = useState('');
     const submitEvent = () => {
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds " + remaining);
+                alert(`The value cannot exceed remaining funds ${currency}${remaining}`);
                 setCost("");
                 return;
             }
